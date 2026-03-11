f2844cd9ce9cc2eb6fdc526eb1028bee3453326d
MafraMP3 <caio_a_mafra@estudante.sesisenai.org.br>
2026-03-11 08:57:26 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+let nome = "Davi Sefnem";
+const pi = 3.14;
+let n1 = 1;
+let n2 = 2;
+
+console.log(nome);
+
+console.log(n1+n2);
+
+function somar(x,y){
+    console.log(x+y);
+}
+function saudacao(){
+    console.log("Olá, meu nome é " + nome);
+}
+somar(5,5);
+saudacao()
