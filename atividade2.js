b31d81cd94cc0c6dc262aa7e0d0a91195c5f4b13
MafraMP3 <caio_a_mafra@estudante.sesisenai.org.br>
2026-03-13 11:06:32 -0300

    ADD  - atividade 3

--- atividade2.js
+++
@@ -1,7 +0,0 @@
-function atividade2(){
-   
-    for(let i = 10; i>=0;i--){
-        alert(i);
-    }
-    alert("Lançamento realizado!");
-}
--- funcoes.js
+++ funcoes.js
@@ -18,6 +18,8 @@
 // somar(5,5);
 // saudacao()
 
+
+
 function atividade1(){
     let soma = 0;
     let continuar = true;   
@@ -40,4 +42,30 @@ function atividade1(){
     alert("A soma dos numeros é: " + soma);
 }
 
-somarNumeros();
+
+
+function atividade2(){
+   
+    for(let i = 10; i>=0;i--){
+        alert(i);
+    }
+    alert("Lançamento realizado!");
+}
+
+
+
+function atividade3(){
+    let gasto =0;
+    let bruto =0;
+    let saldoFim
+
+    for (let i = 1; i <= 12; i++){
+        bruto += parseFloat(prompt("Insira o bruto do " + i +"° mês."));
+        gasto += parseFloat(prompt("Insira o ganho do " + i +"° mês."));
+    }
+    saldoFim = bruto-gasto
+
+    alert("O saldo fincanceiro do ano foi: R$:" + saldoFim);
+    alert("O ganho bruto anual foi de: R$:" + bruto);
+    alert("O gasto anual foi de: R$:" + gasto);
+}
